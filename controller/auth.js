a531d57c67b60b9aee691249c53db1778a7d4623
alexbostock <alex@alexbostock.co.uk>
2017-08-02 15:44:50 +0100

    Create password controller

---
+++ controller/auth.js
@@ -0,0 +1,22 @@
+const passwordLib = require("password-hash-and-salt");
+const uniqid = require("require");
+
+const hash = (password, callback) => {
+	passwordLib.hash(uniqid(), (err, hash) => {
+		if (error) {
+			throw new Error("Failed to hash password!");
+		} else {
+			callback(hash);
+		}
+	}
+}
+
+const verify = (password, hash, callback) => {
+	passwordLib(password).verifyAgainst(hash, (err, verified) => {
+		callback(verified && ! err);
+	}
+}
+
+module.exports.hash = hash;
+module.exports.verify = verify;
+
