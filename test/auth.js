2ecf7b4080fc1aa99dfd8b7f56fa79ab39866aaa
alexbostock <alex@alexbostock.co.uk>
2017-08-02 15:43:35 +0000

    Begin to add tests

---
+++ test/auth.js
@@ -0,0 +1,14 @@
+const auth = require("../controller/auth");
+
+auth.hash("password", (hash) => {
+	console.log(hash);
+
+	auth.verify("password", hash, (ok) => {
+		console.log(ok);
+	});
+
+	auth.verify("Password", hash, (ok) => {
+		console.log(ok);
+	});
+});
+
